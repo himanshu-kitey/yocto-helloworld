df852321e9bb7702c2f7dd091bfe3052a6f964a5
Malyadri Illuri <malyadri@raayansystem.com>
2020-08-23 19:43:22 +0530

    ADDED: km_startup_app

--- HelloWorld.c
+++ HelloWorld.c
@@ -4,8 +4,27 @@
 
 #include "HelloWorld.h"
 
+void LCD_Testcase(void){
+
+        HD44780_ClrScr();
+        HD44780_Str_XY(0,3,"WELCOME TO");
+        HD44780_Str_XY(1,0,"KERNEL MASTERS");
+}
+
+
+void KM_BBB_GPIO_Init(void){
+
+        KM_GPIO_Export(9); // BUZZER
+        KM_GPIO_Direction(9,1);// Set output direction	
+
+} 
+
 int main(int argc, char *argv[])
 {
-  printf("Hello, World...\n");
+        KM_BBB_GPIO_Init();   //Switches, LED, Buzzer Initalization
+        KM_LCD_Init();
+	LCD_Testcase();
+
+
   return 0;
 }
